c8d4dcc1ce276ad121bdb70c4289ebb308efc52d
rode00 <rode222@gmail.com>
2015-04-01 15:14:20 +1000

    test3

--- Swing_1/src/MainFrame.java
+++ Swing_1/src/MainFrame.java
@@ -1,28 +1,32 @@
 import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JTextArea;
 
 public class MainFrame extends JFrame {
 
 	private TextPanel textPanel;
-	private JButton btn;
 	private Toolbar toolbar;
+	private FormPanel formPanel;
 
 	public MainFrame() {
-		super("Hello World");
+		super("	City Cabinetmakers ORM");
 
 		setLayout(new BorderLayout());
 
 		toolbar = new Toolbar();
 		textPanel = new TextPanel();
+		formPanel = new FormPanel();
+
+		toolbar.setStringListener(new StringListener() {
+			public void textEmitted(String text) {
+				textPanel.appendText(text);
+
+			}
+
+		});
 		
-		toolbar.setTextPanel(textPanel);
-
-
+		add(formPanel, BorderLayout.WEST);
 		add(toolbar, BorderLayout.NORTH);
 		add(textPanel, BorderLayout.CENTER);
 
--- Swing_1/src/Toolbar.java
+++ Swing_1/src/Toolbar.java
@@ -2,44 +2,47 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JPanel;
-
 
 public class Toolbar extends JPanel implements ActionListener {
 	private JButton helloButton;
 	private JButton goodbyeButton;
-	
-	private TextPanel textPanel;
-	
+
+	private StringListener textListener;
+
 	public Toolbar() {
-		helloButton = new JButton("Hello");
-		goodbyeButton = new JButton("Goodbye");
-		
+		setBorder(BorderFactory.createEtchedBorder());
+		helloButton = new JButton("Board");
+		goodbyeButton = new JButton("Laminate");
+
 		helloButton.addActionListener(this);
 		goodbyeButton.addActionListener(this);
-		
+
 		setLayout(new FlowLayout(FlowLayout.LEFT));
-		
+
 		add(helloButton);
 		add(goodbyeButton);
 	}
-	
-	public void setTextPanel(TextPanel textPanel) {
-		this.textPanel = textPanel;
-		
+
+	public void setStringListener(StringListener listener) {
+		this.textListener = listener;
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		JButton clicked = (JButton)e.getSource();
-		
+		JButton clicked = (JButton) e.getSource();
+
 		if (clicked == helloButton) {
-			textPanel.appendText("Hello\n");
+			if(textListener != null) {
+				textListener.textEmitted("Hello\n");
+			}
 		}
-		
-		else if(clicked == goodbyeButton){
-			textPanel.appendText("Goodbye\n");
+
+		else if (clicked == goodbyeButton) {
+			textListener.textEmitted("Goodbye\n");
 		}
 	}
 
